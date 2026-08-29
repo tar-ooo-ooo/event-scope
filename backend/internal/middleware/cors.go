5fe50e0a7ded8658d6a1502c4a2e45fffe7d7a56
林郁盛 <sss890925@gmail.com>
2026-08-29 18:41:56 +0800

    feat: 更新 CORS 中介軟體以處理 OPTIONS 請求

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"net/http"
+
 	"event-scope/backend/internal/config"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +15,14 @@ func Cors(cfg config.Config) gin.HandlerFunc {
 			c.Header("Access-Control-Allow-Origin", cfg.FrontendEndpoint)
 			c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 			c.Header("Access-Control-Allow-Headers", "Content-Type")
+
+			if c.Request.Method == http.MethodOptions {
+				c.Status(http.StatusNoContent)
+				c.Abort()
+				return
+			}
 		}
+
+		c.Next()
 	}
 }
